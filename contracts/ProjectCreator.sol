a4b735c4c20c2d87849ddecf5b6a0734b0624389
Nathan Thomas <nwthomas@me.com>
2021-11-10 22:03:52 -0800

    Finish draft of ProjectCreator.sol

--- contracts/ProjectCreator.sol
+++ contracts/ProjectCreator.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.4;
 import "./Ownable.sol";
 import "./Project.sol";
 
-contract ProjectFactory is Ownable {
-  mapping(uint256 => Project) public projectIndexToProject;
-  mapping(address => Project[]) public ownersToProjects;
-
-  constructor() Ownable(msg.sender) {}
+contract ProjectFactory is Ownable(msg.sender) {
+  Project[] public projects;
+  mapping(address => uint256[]) public ownersToProjects;
 
   function createNewProject(
     string memory _name,
     string memory _description,
     uint256 _fundraisingGoal
   ) external payable {
-    // TODO: Write code to declare new owner of Project + pass on msg.value
     Project newProject = new Project(
       _name,
       _description,
       _fundraisingGoal,
       msg.sender
     );
+
+    ownersToProjects[msg.sender].push(projects.length);
+    projects.push(newProject);
   }
 }
